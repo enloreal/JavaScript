f31d002a02072b324528a45e9f8ed33fb9148796
enloreal <arozarg@mail.ru>
2024-11-05 15:22:02 +0300

    10th task

--- 10/script.js
+++ 10/script.js
@@ -1,39 +1,15 @@
 const asyncTimeout = (timeout) => new Promise(res => {
-    setTimeout(() => {
-        res();
-    },timeout);
+  setTimeout(() => {
+      res();
+  }, timeout);
 });
 
-/*
-* FIXME как-то ты перемудрил. Задача элементарно решается через цикл for/of.
-*  Либо на худой конец через рекурсию.
-*   Подсказка: функцию и так возвращает промис, можно сделать ее async
-*  */
-function promiseStack(arr) {
-    return new Promise((res, rej) => {
-      /*
-      * FIXME index - плохое название. За что отвечает эта переменная — не понятно.
-      *   Что бы понять нужно погрузиться в логику кода ниже
-      *  */
-      let index = 0;
-  
-      const executeNext = () => {
-        if (index >= arr.length) {
-          res();
-          return;
-        }
-  
-        const current = arr[index];
-        index++;
-  
-        Promise.resolve(current())
-          .then(executeNext)
-          .catch(rej);
-      };
-  
-      executeNext();
-    });
+// REVIEW сократил функцию, сделал через for/of
+async function promiseStack(arr) {
+  for (const task of arr) {
+      await task();
   }
+}
 
 promiseStack([
   () => asyncTimeout(4000).then(() => console.log(1)),
@@ -41,3 +17,4 @@ promiseStack([
   () => asyncTimeout(1000).then(() => console.log(3)),
   () => asyncTimeout(3000).then(() => console.log(4)),
 ]);
+
