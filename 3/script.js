bc40267db54618c50434021fe372166da1ebf916
enloreal <arozarg@mail.ru>
2024-11-05 15:08:22 +0300

    3rd task

--- 3/script.js
+++ 3/script.js
@@ -1,29 +1,27 @@
-/*
-* Ай-ай-ай https://monosnap.com/file/cq67fLc59Dqz5qGCkkNjHRfAmxp54a
-* Решай сам! Использование чата в целях обучения это очень плохая идея
-*/
 function setInObj(arr, value, obj) {
-    let current = obj;
-    
-    // FIXME старайся использовать for/in и for/of циклы. for/i считаемся устаревшим
-    for (let i = 0; i < arr.length - 1; i++) {
-      const key = arr[i];
+  const copyObj = {...obj};
+  let current = copyObj;
+  
+  // REVIEW заменил for/i на for/of
 
+
+  for (let key of arr) {
+    if (arr.indexOf(key) === arr.length-1) {
+      current[key] = value;
+    } else {
       if (!current[key]) {
         current[key] = {};
       }
-
       current = current[key];
     }
-
-    current[arr[arr.length - 1]] = value;
-    return obj;
   }
 
-  const obj = {};
-  /*
-  * FIXME сделай функцию чистой, js-это больше функциональный язык,
-  *   по этому принципы ФП необходимо соблюдать
-  *  */
-  setInObj(["user", "name"], "Sam", obj);
-  console.log(obj);
+  return copyObj;
+}
+
+const obj = {};
+
+// REVIEW сделал функцию чистой посредством создания копии исходного массива
+
+setInObj(["user", "name"], "Sam", obj);
+console.log(setInObj(["user", "name"], "Sam", obj));
