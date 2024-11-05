68eb8daab2b130c4cd96a70e56736899569d3e7c
enloreal <arozarg@mail.ru>
2024-11-05 15:21:25 +0300

    9th task

--- 9/script.js
+++ 9/script.js
@@ -1,10 +1,9 @@
-// FIXME избегай использования стрелочных функций там, где не требуется сохранение контекста
-const asyncTimeout = (timeout) => new Promise(res => {
-    // FIXME зачем тут объявлять избыточную функцию обертку?
-    setTimeout(() => {
-        res();
-    }, timeout);
-});
+// REVIEW заменил стрелочную функцию на обычную, убрал лишнюю функцию внутри
+function asyncTimeout(timeout) {
+    return new Promise(function(res) {
+        setTimeout(res, timeout);
+    });
+}
 
 
 setTimeout(() => console.log(3), 2000);
@@ -12,3 +11,4 @@ setTimeout(() => console.log(3), 2000);
 console.log(1);
 
 asyncTimeout(1000).then(() => console.log(2));
+
