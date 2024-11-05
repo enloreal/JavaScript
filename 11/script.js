b29873cd183e48491b1c150d334f9caaf47e93ed
enloreal <arozarg@mail.ru>
2024-11-05 15:22:39 +0300

    11th task

--- 11/script.js
+++ 11/script.js
@@ -1,47 +1,33 @@
-// FIXME На основе прошлой задачи проведи рефакторинг самостоятельно
+// REVIEW сделал рефакторинг самостоятельно
 const asyncTimeout = (timeout) => new Promise(res => {
-    setTimeout(() => {
-        res();
-    },timeout);
-  });
-  
-  function promiseStack(promises, count = 1) {
-    return new Promise((resolve, reject) => {
-      const results = [];
-      let running = 0;
-      let index = 0;
-  
-      const executeNext = () => {
-        if (running < count && index < promises.length) {
-          running++;
-          promises[index]()
-            .then((result) => {
-              results[index] = result;
-              running--;
-              if (index < promises.length) {
-                executeNext();
-              }
-              if (running === 0 && index === promises.length) {
-                resolve(results);
-              }
-            })
-            .catch(reject);
-          index++;
-          executeNext();
-        }
-      };
-  
-      executeNext();
-    });
+  setTimeout(() => {
+      res();
+  }, timeout);
+});
+
+async function promiseStack(arr, limit = 1) {
+  const tasks = [];
+
+  for (const task of arr) {
+    const promise = task().then(() => tasks.splice(tasks.indexOf(promise), 1));
+    tasks.push(promise);
+
+    if (tasks.length >= limit) {
+      await Promise.race(tasks);
+    }
   }
-  
-  promiseStack([
-    () => asyncTimeout(4000).then(() => console.log(1)),
-    () => asyncTimeout(4000).then(() => console.log(1)),
-    () => asyncTimeout(2000).then(() => console.log(2)),
-    () => asyncTimeout(2000).then(() => console.log(2)),
-    () => asyncTimeout(1000).then(() => console.log(3)),
-    () => asyncTimeout(1000).then(() => console.log(3)),
-    () => asyncTimeout(3000).then(() => console.log(4)),
-  ], 2);
+
+  await Promise.all(tasks);
+}
+
+promiseStack([
+  () => asyncTimeout(4000).then(() => console.log(1)),
+  () => asyncTimeout(4000).then(() => console.log(1)),
+  () => asyncTimeout(2000).then(() => console.log(2)),
+  () => asyncTimeout(2000).then(() => console.log(2)),
+  () => asyncTimeout(1000).then(() => console.log(3)),
+  () => asyncTimeout(1000).then(() => console.log(3)),
+  () => asyncTimeout(3000).then(() => console.log(4)),
+], 2);
+
   
