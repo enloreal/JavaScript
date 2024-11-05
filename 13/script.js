b71c75502b9e96a45741c8a8afc241eab3679d86
enloreal <arozarg@mail.ru>
2024-11-05 15:24:12 +0300

    13th task

--- 13/script.js
+++ 13/script.js
@@ -1,27 +1,27 @@
 const table = document.getElementById('sampleTable');
-/*
-* FIXME а если количество строк будет динамическим с бека приходить?
-*  Нужно вычислить это значение на основе количества строк в таблице
-* */
-let flag = 3;
+
+// REVIEW теперь количество получаем динамически
+let flag = document.getElementById("body").children.length;
 
 
-/*
-* FIXME функция нарушает принцип единственной ответственности.
-*  Он создает и вставляет строку. А теперь представь что нужно
-*  создание строки переиспользовать в другой функции. Как быть?
-* */
-function insert_Row() {
-  const tdFirst = document.createElement('td');
-  const tdSecond = document.createElement('td');
-  const tr = document.createElement('tr');
-  tdFirst.innerHTML = `Row${flag} cell${flag}`
-  tdSecond.innerHTML = `Row${flag} cell${flag}`
+// REVIEW разбил функцию на две: создание и вставка строки
+function createRow(rowNum) {
+    const tdFirst = document.createElement('td');
+    const tdSecond = document.createElement('td');
+    const tr = document.createElement('tr');
+    
+    tdFirst.innerHTML = `Row${rowNum} cell${1}`;
+    tdSecond.innerHTML = `Row${rowNum} cell${2}`;
+    
+    tr.appendChild(tdFirst);
+    tr.appendChild(tdSecond);
+    
+    return tr;
+}
 
-  tr.appendChild(tdFirst);
-  tr.appendChild(tdSecond);
 
-  table.appendChild(tr);
-
-  flag++;
+function insertRow() {
+    flag++;
+    const newRow = createRow(flag);
+    table.appendChild(newRow);
 }
