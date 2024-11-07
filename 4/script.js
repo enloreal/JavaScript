4a25563c3e77f91272cd1a658a3cff280771616f
enloreal <arozarg@mail.ru>
2024-11-07 16:10:59 +0300

    4th task. Changed the way of making function dirty

--- 4/script.js
+++ 4/script.js
@@ -1,17 +1,25 @@
-// FIXME функция myMap должна вернуть новый массив, а не изменять текущий
+// REVIEW Если я правильно понял твоё задание "для закрепления материала, сделай из чистой функции myMap, грязную функцию", я делал мутацию массива, чтобы функция стала грязной
+
+// REVIEW Убрал мутацию, попытался сделать грязной с помощью измнения внешней переменной + побочного эффекта.
+
+let myMapUses = 0;
+
 Array.prototype.myMap = function(callback) {
 
-    for (let i = 0; i < this.length; i++) {
-      //   тут происходит мутация текущего массива
-      this[i] = callback(this[i], i, this);
-    }
+  const result = [];
 
-    // возвращается тот же массив, а должен вернуться новый
-    return this;
+  for (let i = 0; i < this.length; i++) {
+      result.push(callback(this[i], i, this));
+  }
+
+  myMapUses++; // превращение функции в грязную с помощью изменения глобальной переменной.
+  console.log(`[${arr}] -> [${result}] by myMap has done!`) // превращение функции в грязную с помощью побочного эффекта - вывода результата дополнительно в консоль.
+  return result;
 
 };
-  
-  const arr = [1, 2, 3];
-  const doubled = arr.myMap(item => item * 2);
-  console.log(doubled); // [2, 4, 6]
-  console.log(arr); // [1, 2, 3]
+
+const arr = [1, 2, 3];
+const doubled = arr.myMap(item => item * 2);
+console.log(doubled); // [2, 4, 6]
+console.log(arr); // [1, 2, 3]
+console.log(myMapUses) 
